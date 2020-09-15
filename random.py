9007c238e8c6550828f5d56fd9cbdc3420df4f25
RiyazUlHaque <riyazulhaque922@gmail.com>
2020-09-15 11:20:20 -0400

    Adding new print

--- random.py
+++ random.py
@@ -5,3 +5,4 @@ print ("Hello I am a new line of code")
 res=a+b+c
 print ("This is a new line of code added in the file after cloning")
 print ("Testng on branch")
+print ("Delete branch concept check")
